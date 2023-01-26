4653744f475fe46f5f82f0394d11e241b4aabeac
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2023-01-26 17:10:05 -0300

    fix: add import lost in merge

--- server/src/shared/infra/http/routes/routes.routes.ts
+++ server/src/shared/infra/http/routes/routes.routes.ts
@@ -5,18 +5,20 @@ import { ListRoutesByNeighborhoodController } from "@modules/routes/useCases/lis
 import { ListRoutesByUserController } from "@modules/routes/useCases/listRoutesByUser/ListRoutesByUserController";
 import ensureAuthenticated from "@shared/middlewares/ensureAuthenticated";
 import { Router } from "express";
+import { DeleteRouteController } from "@modules/routes/useCases/deleteRoute/DeleteRouteController";
 
 
 export const routesRoutes = Router()
 
 const createRouteController = new CreateRouteController()
-
 const listRoutesByUserController = new ListRoutesByUserController()
 const listRoutesByNeighborhoodController = new ListRoutesByNeighborhoodController()
+const updateRouteController = new UpdateRouteController()
 const deleteRouteController = new DeleteRouteController()
 
 routesRoutes.post("/", ensureAuthenticated, createRouteController.handle)
 routesRoutes.get("/user", ensureAuthenticated, listRoutesByUserController.handle)
 routesRoutes.get("/neighborhood", ensureAuthenticated, listRoutesByNeighborhoodController.handle)
+routesRoutes.patch("/:id", ensureAuthenticated, updateRouteController.handle)
 routesRoutes.delete("/:id", ensureAuthenticated, deleteRouteController.handle)
 
